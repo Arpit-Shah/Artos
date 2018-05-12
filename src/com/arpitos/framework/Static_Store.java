c7f5ba6e7fc96be5a4dd658f91a4d394006a6df5
ARPITSHAH\arpit <arpit.shah1@gmail.com>
2018-05-12 14:58:35 +1200

    Class description was added

--- src/com/arpitos/framework/Static_Store.java
+++ src/com/arpitos/framework/Static_Store.java
@@ -3,10 +3,17 @@ package com.arpitos.framework;
 import com.arpitos.framework.infra.TestContext;
 import com.arpitos.utils.Convert;
 
+/**
+ * This class provides container which holds all static element of test
+ * framework
+ * 
+ * @author arpit
+ *
+ */
 public class Static_Store {
-	
+
 	public static final String toolName = "Arpitos";
-	
+
 	// Global object storage Variables
 	public static final String GLOBAL_ANNOTATED_TEST_MAP = "ANNOTATED_TEST_MAP";
 	public static final FrameworkConfig FWConfig = new FrameworkConfig(true);
