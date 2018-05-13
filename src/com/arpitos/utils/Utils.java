322b54a1d64ef27bb2ddc70ee6a678e2f59e9dfa
ARPITSHAH\arpit <arpit.shah1@gmail.com>
2018-05-13 15:07:05 +1200

    Stacktrace handling for throwable is added

--- src/com/arpitos/utils/Utils.java
+++ src/com/arpitos/utils/Utils.java
@@ -9,12 +9,12 @@ import com.arpitos.framework.infra.TestContext;
 public class Utils {
 
 	/**
-	 * Writes Print StackTrace on console and log file as per choosen option
+	 * Writes Print StackTrace on console and log file as per chosen option
 	 * 
 	 * @param context
-	 *            test context
+	 *            Test context
 	 * @param e
-	 *            exception
+	 *            Exception
 	 */
 	public static void writePrintStackTrace(TestContext context, Exception e) {
 		StringWriter sw = new StringWriter();
@@ -22,6 +22,37 @@ public class Utils {
 		context.getLogger().error(sw.toString());
 	}
 
+	/**
+	 * Writes Print StackTrace on console and log file as per chosen option
+	 * 
+	 * @param context
+	 *            Test context
+	 * @param e
+	 *            Throwable
+	 */
+	public static void writePrintStackTrace(TestContext context, Throwable e) {
+		StringWriter sw = new StringWriter();
+		e.printStackTrace(new PrintWriter(sw));
+		context.getLogger().error(sw.toString());
+	}
+
+	/**
+	 * Deletes all the content of the directory if present, If directory is not
+	 * present and user has chosen appropriate flag then creates empty
+	 * directory(s)
+	 * 
+	 * <PRE>
+	 * {@code
+	 * Example : cleanDir(new File("./conf", true);
+	 * }
+	 * </PRE>
+	 * 
+	 * @param dir
+	 *            Target directory
+	 * @param createIfNotPresent
+	 *            Boolean flag which enables directory creation if not already
+	 *            present
+	 */
 	public static void cleanDir(File dir, boolean createIfNotPresent) {
 		if (dir.exists() && dir.isDirectory()) {
 			cleanDir(dir);
