f3298b48d5feef98503a8b30279735b19d6c8e8c
ArpitS <arpit.shah@invenco.com>
2018-08-11 10:09:36 +1200

    FWStatic_Store was renamed to FWStaticStore Static context was removed Variable for storing xml based test script file was added TestScript base dir and Script base directory locations were added

--- src/com/artos/framework/FWStaticStore.java
+++ src/com/artos/framework/FWStaticStore.java
@@ -15,20 +15,18 @@
 // OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 package com.artos.framework;
 
-import com.artos.framework.infra.TestContext;
-
 /**
  * This class provides container which holds all static element of test
  * framework
- * 
- * 
- *
  */
-public class FWStatic_Store {
+public class FWStaticStore {
 
 	public static final String TOOL_NAME = "Artos";
+
+	public static final String TESTSCRIPT_BASE_DIR = "./script/";
+	public static final String CONFIG_BASE_DIR = "./conf/";
+
+	public static final String GLOBAL_TEST_SCRIPT_PATH = "TEST_SCRIPT_PATH";
 	public static final String GLOBAL_ANNOTATED_TEST_MAP = "ANNOTATED_TEST_MAP";
 
-	// Global object storage Variables
-	public static TestContext context = new TestContext();
 }
