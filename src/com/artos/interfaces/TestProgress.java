0eb57102e63cc693a7658075b8e36152f487c928
ARPITSHAH\arpit <arpit.shah1@gmail.com>
2018-10-22 22:02:35 +1300

    Interface was extended since parameterised testing is supported, TestProgress will now be notified for parameterised child test case start and finish of execution

--- src/com/artos/interfaces/TestProgress.java
+++ src/com/artos/interfaces/TestProgress.java
@@ -25,73 +25,63 @@ import com.artos.framework.Enums.TestStatus;
 import com.artos.framework.TestObjectWrapper;
 
 /**
- * Interface recommended for any listener which requires to be notified test
- * progress events.
+ * Interface recommended for any listener which requires to be notified test progress events.
  */
 public interface TestProgress {
 
 	/**
-	 * Method is called when test loop execution starts, This method is called
-	 * same number of time as loop count set by user.
+	 * Method is called when test loop execution starts, This method is called same number of time as loop count set by user.
 	 * 
-	 * @param count
-	 *            number of test suite execution loop count
+	 * @param count number of test suite execution loop count
 	 */
 	public void testExecutionLoopCount(int count);
 
 	/**
 	 * Method is called before {@code BeforeTestSuite} method execution starts
 	 * 
-	 * @param description
-	 *            description/name of the test suite
+	 * @param description description/name of the test suite
 	 */
 	public void beforeTestSuiteMethodStarted(String description);
 
 	/**
 	 * Method is called after {@code BeforeTestSuite} method execution finished
 	 * 
-	 * @param description
-	 *            description/name of the test suite
+	 * @param description description/name of the test suite
 	 */
 	public void beforeTestSuiteMethodFinished(String description);
 
 	/**
 	 * Method is called when test suite execution starts
 	 * 
-	 * @param description
-	 *            description/name of the test suite
+	 * @param description description/name of the test suite
 	 */
 	public void testSuiteExecutionStarted(String description);
 
 	/**
 	 * Method is called when test suite execution finishes
 	 * 
-	 * @param description
-	 *            description/name of the test suite
+	 * @param description description/name of the test suite
 	 */
 	public void testSuiteExecutionFinished(String description);
 
 	/**
 	 * Method is called before {@code AfterTestSuite} method execution starts
 	 * 
-	 * @param description
-	 *            description/name of the test suite
+	 * @param description description/name of the test suite
 	 */
 	public void afterTestSuiteMethodStarted(String description);
 
 	/**
 	 * Method is called after {@code AfterTestSuite} method execution finishes
 	 * 
-	 * @param description
-	 *            description/name of the test suite
+	 * @param description description/name of the test suite
 	 */
 	public void afterTestSuiteMethodFinished(String description);
 
 	/**
 	 * Method is called before {@code BeforeTest} method execution starts
 	 * 
-	 * @param t
-	 *            test object wrapper
+	 * @param t test object wrapper
 	 * @see TestObjectWrapper
 	 */
 	public void beforeTestMethodStarted(TestObjectWrapper t);
@@ -99,8 +89,7 @@ public interface TestProgress {
 	/**
 	 * Method is called after {@code BeforeTest} method execution finishes
 	 * 
-	 * @param t
-	 *            test object wrapper
+	 * @param t test object wrapper
 	 * @see TestObjectWrapper
 	 */
 	public void beforeTestMethodFinished(TestObjectWrapper t);
@@ -108,8 +97,7 @@ public interface TestProgress {
 	/**
 	 * Method is called before {@code AfterTest} method execution started
 	 * 
-	 * @param t
-	 *            test object wrapper
+	 * @param t test object wrapper
 	 * @see TestObjectWrapper
 	 */
 	public void afterTestMethodStarted(TestObjectWrapper t);
@@ -117,8 +105,7 @@ public interface TestProgress {
 	/**
 	 * Method is called after {@code AfterTest} method execution finishes
 	 * 
-	 * @param t
-	 *            test object wrapper
+	 * @param t test object wrapper
 	 * @see TestObjectWrapper
 	 */
 	public void afterTestMethodFinished(TestObjectWrapper t);
@@ -126,8 +113,7 @@ public interface TestProgress {
 	/**
 	 * Method is called when test execution starts
 	 * 
-	 * @param t
-	 *            test object wrapper
+	 * @param t test object wrapper
 	 * @see TestObjectWrapper
 	 */
 	public void testExecutionStarted(TestObjectWrapper t);
@@ -135,8 +121,7 @@ public interface TestProgress {
 	/**
 	 * Method is called when test execution finishes
 	 * 
-	 * @param t
-	 *            test object wrapper
+	 * @param t test object wrapper
 	 * @see TestObjectWrapper
 	 */
 	public void testExecutionFinished(TestObjectWrapper t);
@@ -144,19 +129,33 @@ public interface TestProgress {
 	/**
 	 * Method is called when test execution is skipped.
 	 * 
-	 * @param t
-	 *            test object wrapper
+	 * @param t test object wrapper
 	 * @see TestObjectWrapper
 	 */
 	public void testExecutionSkipped(TestObjectWrapper t);
 
 	/**
+	 * Method is called when child test execution starts
+	 * 
+	 * @param t test object wrapper
+	 * @param paramInfo parameterInfo
+	 * @see TestObjectWrapper
+	 */
+	public void childTestExecutionStarted(TestObjectWrapper t, String paramInfo);
+
+	/**
+	 * Method is called when child test execution finishes
+	 * 
+	 * @param t test object wrapper
+	 * @see TestObjectWrapper
+	 */
+	public void childTestExecutionFinished(TestObjectWrapper t);
+
+	/**
 	 * Method is called when user updates test status.
 	 * 
-	 * @param testStatus
-	 *            Test status set by user
-	 * @param msg
-	 *            description/reason provided by user
+	 * @param testStatus Test status set by user
+	 * @param msg description/reason provided by user
 	 * @see TestStatus
 	 */
 	public void testStatusUpdate(TestStatus testStatus, String msg);
@@ -164,46 +163,37 @@ public interface TestProgress {
 	/**
 	 * Method is called when test result is finalised.
 	 * 
-	 * @param testStatus
-	 *            Test status set by user
-	 * @param description
-	 *            bug tracking info (Ticket/JIRA number)
+	 * @param testStatus Test status set by user
+	 * @param description bug tracking info (Ticket/JIRA number)
 	 * @see TestStatus
 	 */
 	public void testResult(TestStatus testStatus, String description);
 
 	/**
-	 * Method is called when test suite execution is finished and summary
-	 * requires to be printed
+	 * Method is called when test suite execution is finished and summary requires to be printed
 	 * 
-	 * @param description
-	 *            description/summary statement
+	 * @param description description/summary statement
 	 */
 	public void testSuiteSummaryPrinting(String description);
 
 	/**
-	 * Method is called when test suite execution is finished and at the end
-	 * failure highlight is required.
+	 * Method is called when test suite execution is finished and at the end failure highlight is required.
 	 * 
-	 * @param description
-	 *            description/summary statement
+	 * @param description description/summary statement
 	 */
 	public void testSuiteFailureHighlight(String description);
 
 	/**
-	 * Method is called when exception is thrown during test suite execution
-	 * which can not be handled on test level
+	 * Method is called when exception is thrown during test suite execution which can not be handled on test level
 	 * 
-	 * @param description
-	 *            Description of an error/exception
+	 * @param description Description of an error/exception
 	 */
 	public void testSuiteException(String description);
 
 	/**
 	 * Method is called when exception is thrown during test case execution
 	 * 
-	 * @param description
-	 *            Description of an error/exception
+	 * @param description Description of an error/exception
 	 */
 	public void testException(String description);
 
