0498164d9a4b8c85a54ae3e1e9358c5d333b36c9
ARPITSHAH\arpit <arpit.shah1@gmail.com>
2019-06-23 12:14:33 +1200

    Removed deprication from the method as it is used my other utilities, so we will continue using this method

--- src/com/artos/utils/Transform.java
+++ src/com/artos/utils/Transform.java
@@ -787,7 +787,6 @@ public class Transform {
 	 * @param length Byte array length
 	 * @return random value byte array
 	 */
-	@Deprecated
 	public byte[] randBytes(int length) {
 		byte[] b = new byte[length];
 		SecureRandom r = new SecureRandom();
