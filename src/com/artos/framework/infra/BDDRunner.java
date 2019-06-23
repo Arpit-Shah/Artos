e7462030d641f104d01140e84607f690bc8e75b8
ArpitS <arpit.shah@invenco.com>
2019-06-24 08:55:55 +1200

    Cosmetics : Java Doc comment fix

--- src/com/artos/framework/infra/BDDRunner.java
+++ src/com/artos/framework/infra/BDDRunner.java
@@ -64,8 +64,9 @@ public class BDDRunner {
 	 * 
 	 * @param context TestContext object
 	 * @param externalListnerClassList external listener class list
-	 * @throws IllegalAccessException 
-	 * @throws InstantiationException 
+	 * @throws IllegalAccessException if the class or its nullary constructor is not accessible.
+	 * @throws InstantiationException if this Class represents an abstract class,an interface, an array class, a primitive type, or void;or if the
+	 *             class has no nullary constructor;or if the instantiation fails for some other reason.
 	 * @see TestContext
 	 * @see TestExecutionEventListener
 	 * @see ExtentReportListener
